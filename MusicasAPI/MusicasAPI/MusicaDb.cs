4975337c1fdf6a38a5cddbfed306d92d198e91a9
Marina Flores <mlbtf@discente.ifpe.edu.br>
2025-05-06 20:16:07 -0300

    🌱 feat: add music listing

---
+++ MusicasAPI/MusicasAPI/MusicaDb.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MusicasAPI;
+
+public class MusicaDb(DbContextOptions<MusicaDb> options) : DbContext(options)
+{
+    public DbSet<Musica> Musicas => Set<Musica>();
+}
